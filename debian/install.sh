3e26a75bfea1fe1e502d619364046c235488577e
Rafael Diniz <rafael@riseup.net>
2023-01-24 01:40:03 +0300

    fixed error in debian dir

--- debian/install.sh
+++ debian/install.sh
@@ -3,11 +3,9 @@
 set -e
 set -x
 rm -rf ${CURDIR}/debian/tmp
-# >> install pre
-# << install pre
-%qmake5_install
-# >> install post
-# << install post
+
+make install
+
 desktop-file-install --delete-original       \
   --dir ${CURDIR}/debian/tmp/usr/share/applications             \
    ${CURDIR}/debian/tmp/usr/share/applications/*.desktop
