db2eb8597a654a6e3ed983a9b03b6624c982006a
Adam Pigg <adam@piggz.co.uk>
2024-12-08 16:24:57 +0000

    Fix loading of boolean values

--- src/settings.cpp
+++ src/settings.cpp
@@ -8,7 +8,11 @@ Settings::Settings(QObject *parent) : QObject(parent)
 
 QVariant Settings::get(const QString &group, const QString &setting, QVariant def)
 {
-    return m_settings.value(group + QStringLiteral("/") + setting, def);
+    QVariant v = m_settings.value(group + QStringLiteral("/") + setting, def);
+    if (v.toString().toLower() == QStringLiteral("false") || v.toString().toLower() == QStringLiteral("true")) {
+        return v.toBool();
+    }
+    return v;
 }
 
 void Settings::set(const QString &group, const QString &setting, QVariant value)
